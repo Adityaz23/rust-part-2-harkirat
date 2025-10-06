de5b9a4c369b818c95fc579577bec933300354b5
apple <apple@apples-MacBook-Air.local>
2025-10-06 09:40:22 +0530

    Strings,functions and conditional are done

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,10 @@
-fn main() {
-    println!("Hello, world!");
-    /*
-        In this rust file we are doing recap of the harkirat singh course of rust part-1 and now after that we will do the part-2 of the course and then in the 3rd part we will do the solana wokshops and then after the recap of the part-1 we will make 2 projects with it like cli and http connection based project.
-
-        Recalling Part-1 of Harkirat Singh's Rust Course:
-     */
-    let a = 12;
-    let b = 32;
-    let c = a+b;
-    println!("The sum of a and b is: {}",c);
-    // outermain();
-    println!("The number is: {}",is_even(2324));
-}
 // # Now we are practising some basic things which will recap us the part-1 of the harkirat rust series ->
 // Q. Write a function is_even that taked number as input and returns true if the number is even else false.
 
-fn is_even(num: i64)-> bool{
-    if num % 2 == 0{
+fn is_even(num: u64) -> bool {
+    if num % 2 == 0 {
         return true;
-    }
-    else {
+    } else {
         return false;
     }
 }
@@ -34,3 +19,90 @@ fn is_even(num: i64)-> bool{
 //         println!("The number is not even, hence it is odd!");
 //     }
 // }
+
+// Q. Write a function fib to find the fibonacci number of a given number n it takes as input.
+
+// this is the recursive version of the fibonacci series.
+
+// fn fib(fb: u64)-> u64{
+//     if fb==0{
+//         return 0;
+//     }
+//     else if fb == 1 {
+//         return 1;
+//     }
+//     else {
+//         return fib(fb-1)+fib(fb-2);
+//     }
+// }
+
+// another way -> This is iterative version of the fibonacci series. This is more efficient than the recursive one.because it time complexity is O(n) and space complexity is O(1).
+fn fib(num: u64) -> u64 {
+    let mut first = 0;
+    let mut second = 1;
+    if num == 0 {
+        return first;
+    }
+    if num == 1 {
+        return second;
+    }
+    for _ in 1..num {
+        let temp = second;
+        second = first + second;
+        first = temp;
+    }
+    return second;
+}
+fn main() {
+    println!("Hello, world!");
+    /*
+       In this rust file we are doing recap of the harkirat singh course of rust part-1 and now after that we will do the part-2 of the course and then in the 3rd part we will do the solana wokshops and then after the recap of the part-1 we will make 2 projects with it like cli and http connection based project.
+
+       Recalling Part-1 of Harkirat Singh's Rust Course:
+    */
+    let a = 12;
+    let b = 32;
+    let c = a + b;
+    println!("The sum of a and b is: {}", c);
+    // outermain();
+    println!("The number is: {}", is_even(23244234));
+    // println!("The fibonacci number is: {}", fib(20))
+    // let x = 23;
+    // println!("{}",x);
+    println!("The fib number is: {}", fib(28));
+    let my_string = String::from("Harkirat rust series part-2");
+    let length = get_string_length(&my_string);
+    println!("The length of the the stirng is: {}",length);
+}
+
+/*
+fn fib(num:u64)->u64{
+    let mut first =0;
+    let second = 1;
+    if (num==0){
+    return first;}
+}
+    if (num==1){
+    return second;
+
+    for i in 1..num{
+    let temp = second;
+    second = first+second;
+    first = temp;
+    }
+*/
+
+/*
+    Q. Write a function get_String_length that takes a string as input and returns the length of the string.
+    let my_string = String::from ("Hello Okay");
+    let length = get_String_length(&my_String);
+    println!("Length {}",length);
+
+    fn get_String_length(s &str)->usize{
+    s.chars().count() or return s..chars()count();
+    }
+*/
+
+fn get_string_length(s: &str)-> usize{
+    s.chars().count()
+}
